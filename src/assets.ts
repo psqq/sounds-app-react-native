6e633c530e6b981033fa39c9bf8b4edaf7d1793e
psqq <pestov-sa@ya.ru>
2020-05-30 14:00:39 +0300

    Assets tree

--- src/assets.ts
+++ src/assets.ts
@@ -14,3 +14,29 @@ export const SOUNDS_02_STRONG_WIND_SOUND: rrtfr = require('./assets/sounds/02-st
 export const SOUNDS_03_THUNDER_ICON: rrtfr = require('./assets/sounds/03-thunder/icon.png');
 export const SOUNDS_03_THUNDER_PREVIEW: rrtfr = require('./assets/sounds/03-thunder/preview.jpg');
 export const SOUNDS_03_THUNDER_SOUND: rrtfr = require('./assets/sounds/03-thunder/sound.mp3');
+
+export const ASSETS_TREE = {
+  icon: {
+    coffee_cup: ICON_COFFEE_CUP,
+    day: ICON_DAY,
+    eye: ICON_EYE,
+    yoga: ICON_YOGA,
+  },
+  sounds: {
+    '01_rain': {
+      icon: SOUNDS_01_RAIN_ICON,
+      preview: SOUNDS_01_RAIN_PREVIEW,
+      sound: SOUNDS_01_RAIN_SOUND,
+    },
+    '02_strong_wind': {
+      icon: SOUNDS_02_STRONG_WIND_ICON,
+      preview: SOUNDS_02_STRONG_WIND_PREVIEW,
+      sound: SOUNDS_02_STRONG_WIND_SOUND,
+    },
+    '03_thunder': {
+      icon: SOUNDS_03_THUNDER_ICON,
+      preview: SOUNDS_03_THUNDER_PREVIEW,
+      sound: SOUNDS_03_THUNDER_SOUND,
+    },
+  },
+};
--- tools/make-assets-consts.js
+++ tools/make-assets-consts.js
@@ -1,6 +1,23 @@
 const dirTree = require('directory-tree');
 
 let consts = [];
+let tree = {};
+
+function addToTree(varName, pathFromAssets) {
+  const treePath = pathFromAssets
+    .slice('./assets/'.length, pathFromAssets.length - 4)
+    .split('/')
+    .map((s) => s.replace(/[^\w]/g, '_'));
+  console.log(treePath, varName);
+  let obj = tree;
+  for (const pathChunk of treePath.slice(0, treePath.length - 1)) {
+    if (!obj[pathChunk]) {
+      obj[pathChunk] = {};
+    }
+    obj = obj[pathChunk];
+  }
+  obj[treePath[treePath.length - 1]] = varName;
+}
 
 const assetsTree = dirTree('./src/assets', null, (item, PATH, stats) => {
   const varName = item.path
@@ -9,7 +26,20 @@ const assetsTree = dirTree('./src/assets', null, (item, PATH, stats) => {
     .toUpperCase();
   const pathFromAssets =
     './' + item.path.slice('src/'.length).replace(/\\/g, '/');
+  addToTree(varName, pathFromAssets);
   consts.push(`export const ${varName}: rrtfr = require('${pathFromAssets}');`);
 });
 
 console.log(consts.join('\n'));
+
+console.log();
+
+console.log(
+  'export const tree = ' +
+    JSON.stringify(tree, null, 4)
+      //.replace(/"/g, '') +
+      .split('\n')
+      .map((s) => s.replace(/"(\w+)",?$/g, '$1,'))
+      .join('\n') +
+    ';',
+);
