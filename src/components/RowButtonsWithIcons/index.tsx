e7d1c82caf9041740f6e8b616e10104efac4f65b
psqq <pestov-sa@ya.ru>
2020-06-01 16:14:16 +0300

    Row buttons with icon

---
+++ src/components/RowButtonsWithIcons/index.tsx
@@ -0,0 +1,61 @@
+import React, {FunctionComponent} from 'react';
+import {StyleSheet, Image, View, Text} from 'react-native';
+import {FixedGrid} from '../FixedGrid';
+
+type Props = {
+  buttons: {id: string; title: string; icon: number}[];
+  onPress?: (id: string) => void;
+};
+
+export const RowButtonsWithIcons: FunctionComponent<Props> = ({
+  buttons = [],
+}) => {
+  return (
+    <View style={styles.container}>
+      <FixedGrid
+        cols={buttons.length}
+        items={buttons.map((btn) => (
+          <View style={styles.btnContainer}>
+            <View style={styles.iconContainer}>
+              <Image source={btn.icon} style={styles.icon} />
+            </View>
+            <Text style={styles.text}>{btn.title}</Text>
+          </View>
+        ))}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 50,
+    width: '100%',
+    marginTop: 25,
+    marginBottom: 25,
+  },
+  btnContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 35,
+    height: 35,
+  },
+  iconContainer: {
+    backgroundColor: '#192f6a',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginBottom: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 12,
+    textAlign: 'center',
+    color: '#fff',
+  },
+});
--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,15 +1,41 @@
 import React, {FunctionComponent} from 'react';
+import shortid from 'shortid';
 
 import {GradientBackground} from '../../components/GradientBackground';
 import {config} from '../../config';
 import {TransparentStatusBar} from '../../components/TransparentStatusBar';
 import {ContainerWithoutStatusBar} from '../../components/ContainerWithoutStatusBar';
+import {RowButtonsWithIcons} from '../../components/RowButtonsWithIcons';
 import {Title} from '../../components/Title';
 import {MainNavigationProp} from 'src/router';
+import {ICON_DAY, ICON_COFFEE_CUP, ICON_EYE, ICON_YOGA} from '../../assets';
 
 type Props = {
   navigation: MainNavigationProp;
 };
+
+const buttons = [
+  {
+    id: shortid(),
+    title: 'Сон',
+    icon: ICON_DAY,
+  },
+  {
+    id: shortid(),
+    title: 'Расслабиться',
+    icon: ICON_COFFEE_CUP,
+  },
+  {
+    id: shortid(),
+    title: 'Работа',
+    icon: ICON_EYE,
+  },
+  {
+    id: shortid(),
+    title: 'Медитация',
+    icon: ICON_YOGA,
+  },
+];
 
 export const Main: FunctionComponent<Props> = ({}) => {
   return (
@@ -17,6 +43,7 @@ export const Main: FunctionComponent<Props> = ({}) => {
       <TransparentStatusBar />
       <ContainerWithoutStatusBar>
         <Title text="Звуки дождя" />
+        <RowButtonsWithIcons buttons={buttons} />
       </ContainerWithoutStatusBar>
     </GradientBackground>
   );
