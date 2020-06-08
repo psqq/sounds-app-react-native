692ae28abd11383d48b9c144b5d8e4f6b69dd3f5
psqq <pestov-sa@ya.ru>
2020-06-08 11:39:43 +0300

    Config for tabbar

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,11 @@
 import {getStatusBarHeight} from 'react-native-status-bar-height';
 
 export const config = {
-  get backgroundGradient() {
-    return ['#192f6a', '#3b5998', '#4c669f'];
+  backgroundGradient: ['#192f6a', '#3b5998', '#4c669f'],
+  tabBar: {
+    iconSize: 20,
+    textFontSize: 12,
+    height: 60,
   },
   paddingForTabBar: 80,
   statusBarHeight: getStatusBarHeight(),
