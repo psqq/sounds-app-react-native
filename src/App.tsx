7d1c6d3f5513a20f7d7abaeea060d00bd7ba9825
psqq <pestov-sa@ya.ru>
2020-06-08 11:20:22 +0300

    Update app initialization method

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,25 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useEffect} from 'react';
+import React from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import {Provider, connect} from 'react-redux';
+import {connect, Provider} from 'react-redux';
 import {Stack} from './router';
 import {Main} from './screens/Main';
 import {SoundWithTimer} from './screens/SoundWithTimer';
 import {UserWishes} from './screens/UserWishes';
-import {store, RootState, TypeOfConnect} from './store';
+import {RootState, store, TypeOfConnect} from './store';
 import {initAppAction} from './store/app/types';
 import {isAppInitedSelector} from './store/selectors';
 
-const storeEnhancer = connect(
-  (state: RootState) => ({
-    wishesLoaded: state.userWishes.loaded,
-    isAppInited: isAppInitedSelector(state),
-  }),
-  (dispatch) => {
-    return {
-      init: () => dispatch(initAppAction()),
-    };
-  },
-);
+const storeEnhancer = connect((state: RootState) => ({
+  wishesLoaded: state.userWishes.loaded,
+  isAppInited: isAppInitedSelector(state),
+}));
 
 type Props = TypeOfConnect<typeof storeEnhancer>;
 
+// Main application component
 let App = storeEnhancer((props: Props) => {
-  const {init, wishesLoaded, isAppInited} = props;
-  useEffect(() => {
-    if (!isAppInited) {
-      init();
-    }
-    if (isAppInited) {
-      SplashScreen.hide();
-    }
-  }, [init, isAppInited]);
+  const {wishesLoaded, isAppInited} = props;
   if (!isAppInited) {
     return <></>;
   }
@@ -57,6 +43,16 @@ let App = storeEnhancer((props: Props) => {
   );
 });
 
+// Init app
+store.dispatch(initAppAction());
+const unsubscribeOnAppInit = store.subscribe(() => {
+  if (isAppInitedSelector(store.getState())) {
+    SplashScreen.hide();
+    unsubscribeOnAppInit();
+  }
+});
+
+// Return app with provided redux storage
 export default () => (
   <Provider store={store}>
     <App />
